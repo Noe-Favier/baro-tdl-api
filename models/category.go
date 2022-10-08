b415b428ed85923e8b51c918e65f493f6054c568
Noé Favier <noe.favier@outlook.com>
2022-10-08 14:57:19 +0200

    fix(element/category): id is private

--- models/category.go
+++ models/category.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 type Category struct {
 	gorm.Model
 
-	ID    uint   `json:"id" gorm:"primary_key"`
+	ID    uint   `json:"-" gorm:"primary_key"`
 	Label string `json:"label"`
 	Code  string `json:"code" gorm:"unique;not null"`
 
--- models/element.go
+++ models/element.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 type Element struct {
 	gorm.Model
 
-	ID      uint   `json:"id" gorm:"primary_key"`
+	ID      uint   `json:"-" gorm:"primary_key"`
 	Label   string `json:"label"`
 	Checked bool   `json:"checked"`
 	Code    string `json:"code" gorm:"unique;not null"`
