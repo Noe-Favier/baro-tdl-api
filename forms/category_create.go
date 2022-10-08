81705d17eb325dca784c3920b46438282055cbaf
Noé Favier <noe.favier@outlook.com>
2022-10-08 15:03:35 +0200

    fix(category create): renamed "created_by_username" by "creator"

--- forms/category_create.go
+++ forms/category_create.go
@@ -2,5 +2,5 @@ package forms
 
 type FormCreateCategory struct {
 	Label             string `json:"label"`
-	CreatedByUsername string `json:"created_by_username"`
+	CreatedByUsername string `json:"creator"`
 }
